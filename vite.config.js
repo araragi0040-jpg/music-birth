6b0adb08e950c768b9d083107260e5a10518ee1a
araragi0040-jpg <araragi0040@gmail.com>
2026-07-03 15:54:29 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "./",
+export default {
+  base: './',
   build: {
-    target: "es2020",
+    target: 'es2020',
   },
-});
+};
